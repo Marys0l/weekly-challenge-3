4edf07849c93ce06aee6b06dd7fa9d7e87f76368
Marys0l <ednagalindo1@gmail.com>
2020-11-01 16:47:07 -0500

    more code,testing functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,31 @@
 function generatePassword() {
   
   var passwordCharacters = [];
-
-  
   var specialCharStr = " !@#$%^&* ";
-
-  var includeLowercase = false;
-  var includeUppercase = false;
+  var includeLowercase = " abcdefeghijklmnopqrstuvwxyz ";
+  var includeUppercase = " ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
   var includeNumeric = false;
   var includeSpecial = false;
-
-  
+  var passwordLength = prompt("Please type the desired length of your password. Password must be greater than 8 characters and less than 128 characters");
+  console.log(passwordLength);
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    includeLowercase = confirm("Would you like your password to contatain lowercase letters");
+    includeUppercase = confirm("Would you like your password to contatain uppercase characters?");
+    includeNumeric = confirm("Would you like your password to contatain numbers?");
+    includeSpecial = confirm("Would you like your password to contatain special characters?");
+  }
+  if (includeLowercase) {
+    for (var i = 97; i <= 122; i++) {
+      passwordCharacters.push(String.fromCharCode(i));
+    }
+  }
+  if (includeUppercase) {
+    for (var i = 65; i <= 90; i++) {
+      passwordCharacters.push(String.fromCharCode(i));
+    }
+  }
+  if (includeNumeric) {
+    for (var i = 0; i <= 8; i++) {
+      passwordCharacters.push(i.toString());
+    }
+  }
