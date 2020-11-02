ac775f396df4fb1054f27c1d1fdcf7e8c4ab03a0
Marys0l <ednagalindo1@gmail.com>
2020-11-01 20:06:49 -0500

    3 hours later, more code, 1 issue left--password has to be between 8-128 characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,57 @@
 function generatePassword() {
   
   var passwordCharacters = [];
-  var specialCharStr = " !@#$%^&* ";
-  var includeLowercase = " abcdefeghijklmnopqrstuvwxyz ";
-  var includeUppercase = " ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-  var includeNumeric = false;
+  //use only these special characters 
+  var usespecial = "!@#$%^&*";
+  var includeLowercase = false;
+  var includeUppercase = false;
+  var includeNumbers = false;
   var includeSpecial = false;
-  var passwordLength = prompt("Please type the desired length of your password. Password must be greater than 8 characters and less than 128 characters");
+  var passwordLength = prompt("MUST type a digit between 8 and 128 , then click OK");
   console.log(passwordLength);
-  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+  if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecial) {
     includeLowercase = confirm("Would you like your password to contatain lowercase letters");
     includeUppercase = confirm("Would you like your password to contatain uppercase characters?");
-    includeNumeric = confirm("Would you like your password to contatain numbers?");
+    includeNumbers = confirm("Would you like your password to contatain numbers?");
     includeSpecial = confirm("Would you like your password to contatain special characters?");
   }
+  //lowercase letters are codes 97-122
   if (includeLowercase) {
     for (var i = 97; i <= 122; i++) {
       passwordCharacters.push(String.fromCharCode(i));
     }
   }
+  //uppercase letters are codes 65-90
   if (includeUppercase) {
     for (var i = 65; i <= 90; i++) {
       passwordCharacters.push(String.fromCharCode(i));
     }
   }
-  if (includeNumeric) {
-    for (var i = 0; i <= 8; i++) {
+  //use digits 1-9
+  if (includeNumbers) {
+    for (var i = 0; i <= 9; i++) {
       passwordCharacters.push(i.toString());
     }
   }
+  if (includeSpecial) {
+    var specialArray = usespecial.split("");
+    passwordCharacters = passwordCharacters.concat(specialArray);
+  }
+  console.log(passwordCharacters);
+  var finalpassword = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * passwordCharacters.length);
+    // random generation of password
+    finalpassword += passwordCharacters[randomIndex];
+  }
+  // password will populate in Your Secure Password box
+  return finalpassword;
+}
+var generateBtn = document.querySelector("#generate");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+
+}
+generateBtn.addEventListener("click", writePassword);
